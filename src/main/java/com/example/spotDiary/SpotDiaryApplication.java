a5173ee460b8fcbe8b805ced61e83f3fbe6e10cf
haryang Park <haryang92@gmail.com>
2023-03-31 15:57:20 +0900

    Initial Commit

---
+++ src/main/java/com/example/spotDiary/SpotDiaryApplication.java
@@ -0,0 +1,13 @@
+package com.example.spotDiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotDiaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotDiaryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spotDiary/SpotDiaryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spotDiary;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotDiaryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
